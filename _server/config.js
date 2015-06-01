ad305f6775849693a0bc1a984802c77e6d8ba5d4
Asa Jones [codechaotic] <thinkinfractals@gmail.com>
2015-06-01 14:38:10 -0400

    fix document width

--- _server/config.js
+++ _server/config.js
@@ -2,7 +2,9 @@
 
   var exports = module.exports = {};
 
-  exports.mongo_url = process.env.MONGO_URL || 'mongodb://' + process.env.MONGO_PORT_27017_TCP_ADDR + '/test';
+  exports.mongo_url = process.env.MONGO_URL ||
+    'mongodb://' + process.env.MONGO_PORT_27017_TCP_ADDR + '/test'
+
   exports.port = process.env.PORT || 8080
 
 })();
